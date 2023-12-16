d09e01fb184ac13cca848ffe939f78fce154dc4b
Elizer Ponio Jr <elizerponiojr@gmail.com>
2023-12-16 17:44:11 +0800

    updated sample

--- index_server.py
+++ index_server.py
@@ -8,6 +8,12 @@ from multiprocessing import Lock
 from multiprocessing.managers import BaseManager
 from llama_index import SimpleDirectoryReader, GPTVectorStoreIndex, Document, ServiceContext, StorageContext, load_index_from_storage
 
+# ---- added
+import torch
+from llama_index.prompts import PromptTemplate
+from llama_index.llms import HuggingFaceLLM
+# --- added
+
 index = None
 stored_docs = {}
 lock = Lock()
@@ -19,8 +25,37 @@ pkl_name = "stored_documents.pkl"
 def initialize_index():
     """Create a new global index, or load one from the pre-set path."""
     global index, stored_docs
-    
-    service_context = ServiceContext.from_defaults(chunk_size_limit=512)
+
+    # ------- Custom LLM
+
+    system_prompt = """<|SYSTEM|># StableLM Tuned (Alpha version)
+    - StableLM is a helpful and harmless open-source AI language model developed by StabilityAI.
+    - StableLM is excited to be able to help the user, but will refuse to do anything that could be considered harmful to the user.
+    - StableLM is more than just an information source, StableLM is also able to write poetry, short stories, and make jokes.
+    - StableLM will refuse to participate in anything that could harm a human.
+    """
+
+    # This will wrap the default prompts that are internal to llama-index
+    query_wrapper_prompt = PromptTemplate("<|USER|>{query_str}<|ASSISTANT|>")
+
+    llm = HuggingFaceLLM(
+        context_window=4096,
+        max_new_tokens=256,
+        generate_kwargs={"temperature": 0.7, "do_sample": False},
+        system_prompt=system_prompt,
+        query_wrapper_prompt=query_wrapper_prompt,
+        tokenizer_name="StabilityAI/stablelm-tuned-alpha-3b",
+        model_name="StabilityAI/stablelm-tuned-alpha-3b",
+        device_map="auto",
+        stopping_ids=[50278, 50279, 50277, 1, 0],
+        tokenizer_kwargs={"max_length": 4096},
+        # uncomment this if using CUDA to reduce memory usage
+        # model_kwargs={"torch_dtype": torch.float16}
+    )
+
+    # ------- Custom LLM
+
+    service_context = ServiceContext.from_defaults(chunk_size_limit=512, llm=llm)
     with lock:
         if os.path.exists(index_name):
             index = load_index_from_storage(StorageContext.from_defaults(persist_dir=index_name), service_context=service_context)
